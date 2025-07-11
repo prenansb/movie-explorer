ee01c248b8ee14f6d7dff27be015f44c41d4e997
prenansb <prenansb@gmail.com>
2025-07-11 18:37:27 -0300

    feat: implement static parameter generation for movie pages using fetched movie data

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,5 +1,12 @@
 import { Suspense } from 'react'
+import { fetchMovies } from '@/lib/api'
 import { MovieDetails } from './_components/movie-details'
+
+export async function generateStaticParams() {
+  const { results } = await fetchMovies({ page: 1 })
+
+  return results.map(movie => ({ id: movie.id.toString() }))
+}
 
 export default async function MoviePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
